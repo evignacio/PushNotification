3a74a417d72f58ea12cc9b63a1e2c31fde7fd47f
Evandro <evandro.ignacio@fcamara.com.br>
2020-06-12 00:41:51 -0300

    configuração dos beans

--- src/main/java/com/example/demo/PushNotificationApplication.java
+++ src/main/java/com/example/demo/PushNotificationApplication.java
@@ -1,13 +1,59 @@
 package com.example.demo;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class PushNotificationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PushNotificationApplication.class, args);
+	}
+
+	@Bean
+	public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+		return FirebaseMessaging.getInstance(firebaseApp);
+	}
+
+	@Bean
+	public FirebaseApp firebaseApp() throws IOException {
+
+		Path root = FileSystems.getDefault().getPath("").toAbsolutePath();
+		Path filePath = Paths.get(root.toString(),"src", "main", "resources", "hack-c8271-firebase-adminsdk-x6hh4-3af6eec313.json");
+
+		System.out.println(filePath.toString());
+		FileInputStream serviceAccount =
+				new FileInputStream(filePath.toString());
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.setDatabaseUrl("https://hack-c8271.firebaseio.com")
+				.build();
+
+		return FirebaseApp.initializeApp(options);
+	}
+
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("http://localhost:8888");
+			}
+		};
 	}
 
 }
